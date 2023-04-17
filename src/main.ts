3d207634d5c50a58b5358fecd5d879fb38f2953e
Lucas Monteiro Inacio <lmsupport@outlook.com>
2023-04-17 22:21:33 +0100

    :recycle:: update main file and remove default from app module

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { PrismaService } from '@prisma-orm/prisma.service';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  // Prisma configs
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
 bootstrap();
