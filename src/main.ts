c9953d193fb84b5a01f3238694b8c796446fffb8
Lucas Monteiro Inacio <lmsupport@outlook.com>
2023-05-29 18:09:57 +0100

    :wrench:: add global prefix to api

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
+  // Add Global Prefix
+  app.setGlobalPrefix('api/v1');
+  
   // Prisma configs
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
